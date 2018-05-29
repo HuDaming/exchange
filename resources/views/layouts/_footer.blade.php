23ce061381a2c315742243a66f917ed9a26c3db1
Hudaming <40834106@qq.com>
2018-05-29 06:51:41 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/1076478212?is_all=1" target="_blank">Apple 情绪化</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:apple@5usport.com">联系我们</a></p>
+    </div>
+</footer>
